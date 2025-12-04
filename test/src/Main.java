8e8ce215513e225f73e5dcdc5f322aaf14158cbe
Om Bharadwaj <omdev434@gamil.com>
2025-12-04 17:34:14 +0530

    lAB `1`

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Name:- Om Bharadwaj");
+        System.out.println("Roll Number:- 24052629");
+        System.out.println("Section:- CSE 48");
+        System.out.println("Branch:- B.Tech CSE ");
     }
 }
